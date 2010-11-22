99c13f71c7d8ad32bfb81247e42d6f96d9f28089
loveecody <loveecody@foxmail.com>
2010-11-22 14:40:35 +0000

    内容：初始化数据 提交者：jc

---
+++ dbscript/prod_pack_type.sql
@@ -0,0 +1,7 @@
+INSERT INTO `prod_pack_type` VALUES (1,'Ã˙π€“Ù…´œ„‘œŒ∂','100*60*63*1(mm/kg)','aaaa','20100000001.jpg','19','1');
+INSERT INTO `prod_pack_type` VALUES (2,'‘œ≤Ë2','130*70*80*1(mm/kg)','bbb','20100001110.jpg','20','1');
+INSERT INTO `prod_pack_type` VALUES (3,'¡È÷•≤Ë','80*50*40*0.5(mm/kg)','acccaaa','20100003300.jpg','35','1');
+INSERT INTO `prod_pack_type` VALUES (4,'‘œ≤Ë4','150*80*90*2(mm/kg)','ddd','20100600000.jpg','40','1');
+INSERT INTO `prod_pack_type` VALUES (5,'‘œ≤Ë5','60*70*30*0.5(mm/kg)','eee','20100008790.jpg','70','0');
+INSERT INTO `prod_pack_type` VALUES (6,'‘œ≤Ë6','122*62*63*1(mm/kg)','fff','20105600000.jpg','35','1');
+INSERT INTO `prod_pack_type` VALUES (7,'‘œ≤Ë7','120*60*60*1(mm/kg)','gggg','20100000034.jpg','50','1');

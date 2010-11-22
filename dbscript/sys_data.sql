88fab462d3a2c8c969f693d63d5dbc0060192a06
loveecody <loveecody@foxmail.com>
2010-11-22 14:41:55 +0000

    内容：初始化数据 提交者：jc

---
+++ dbscript/sys_data.sql
@@ -0,0 +1,19 @@
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE */;
+/*!40101 SET SQL_MODE='STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES */;
+/*!40103 SET SQL_NOTES='ON' */;
+
+
+INSERT INTO `sys_data` VALUES (3,'资讯类别',0,'0');
+INSERT INTO `sys_data` VALUES (31,'市场营销',3,'0');
+INSERT INTO `sys_data` VALUES (32,'国内资讯',3,'0');
+INSERT INTO `sys_data` VALUES (33,'环球茶市',3,'0');
+INSERT INTO `sys_data` VALUES (34,'政策法规',3,'0');
+INSERT INTO `sys_data` VALUES (35,'展会报到',3,'0');
+INSERT INTO `sys_data` VALUES (36,'茶叶种植',3,'0');
+INSERT INTO `sys_data` VALUES (37,'茶人茶事',3,'0');
+INSERT INTO `sys_data` VALUES (38,'品牌营销',3,'0');
+INSERT INTO `sys_data` VALUES (39,'促销资讯',3,'0');
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;

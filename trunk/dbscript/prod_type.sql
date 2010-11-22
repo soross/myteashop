7f492fffc50a1f4e95ef60d3cc7d2ea499a07c05
loveecody@foxmail.com <loveecody@foxmail.com@6d9ef304-1be9-e63d-85db-14d8c94b65ce>
2010-11-22 14:41:20 +0000

    内容：初始化数据 提交者：jc

---
+++ trunk/dbscript/prod_type.sql
@@ -0,0 +1,16 @@
+INSERT INTO `prod_type` VALUES (1,'功能分类',0);
+INSERT INTO `prod_type` VALUES (2,'保健茶系列',1);
+INSERT INTO `prod_type` VALUES (3,'礼品铁观音',1);
+INSERT INTO `prod_type` VALUES (4,'商务茶系列',1);
+INSERT INTO `prod_type` VALUES (5,'听装铁观音',1);
+INSERT INTO `prod_type` VALUES (6,'习性分类',0);
+INSERT INTO `prod_type` VALUES (7,'风味铁观音',6);
+INSERT INTO `prod_type` VALUES (8,'清香铁观音',6);
+INSERT INTO `prod_type` VALUES (9,'茶种分类',0);
+INSERT INTO `prod_type` VALUES (10,'闽北乌龙',9);
+INSERT INTO `prod_type` VALUES (11,'闽南乌龙',9);
+INSERT INTO `prod_type` VALUES (12,'台湾乌龙 ',9);
+INSERT INTO `prod_type` VALUES (13,'绿茶系列',9);
+INSERT INTO `prod_type` VALUES (14,'红茶茶系列',9);
+INSERT INTO `prod_type` VALUES (15,'普洱茶系列',9);
+

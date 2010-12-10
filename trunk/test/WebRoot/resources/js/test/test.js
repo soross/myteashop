777d19e7d589388a9491c2a1995f729d2aeb17c8
fmlzly@vip.qq.com <fmlzly@vip.qq.com@6d9ef304-1be9-e63d-85db-14d8c94b65ce>
2010-12-10 02:29:44 +0000

    无限级菜单递归方法初步测试

--- trunk/test/WebRoot/resources/js/test/test.js
+++ trunk/test/WebRoot/resources/js/test/test.js
@@ -28,13 +28,36 @@ function getMenu(id) {
 				}
 				else
 				{
-					var xml = "id: "+json.ID+"<br>";
-					xml = xml + "name: "+json.NAME+"<br>";
-					xml = xml +"url: "+json.URL+"<br>";
-					xml = xml +"pid: "+json.PID+"<br>";
+//					var xml = "id: "+json.ID+"<br>";
+//					xml = xml + "name: "+json.NAME+"<br>";
+//					xml = xml +"url: "+json.URL+"<br>";
+//					xml = xml +"pid: "+json.PID+"<br>";
+					var xml = getMenuList(json.LIST,0);
+					alert(xml);
 					document.getElementById('Menu').innerHTML = xml;
 				}
 			}
 		}
 	});
  }
+
+function getMenuList(list,id)
+{
+	var xml = '';
+	   for(var i =0;i<list.length;i++)
+       {
+		   var str = '';
+		   for(var j =0;j<id;j++)
+		   {
+			   str = str+'----';
+		   }
+		   xml = xml+ str +"id: "+list[i].ID+",name: "+list[i].NAME+",url: "+list[i].URL+",pid: "+list[i].PID+"<br>";
+		   if(list[i].SMENU.length !=0)
+           {
+        	   alert(list[i].SMENU.length);
+        	   xml = xml + getMenuList(list[i].SMENU,id+1);
+           }
+               
+       }
+	   return xml;
+}

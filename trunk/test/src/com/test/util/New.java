985d9631a3ba44c2612fe9259fbf6d2f3cee8541
loveecody@foxmail.com <loveecody@foxmail.com@6d9ef304-1be9-e63d-85db-14d8c94b65ce>
2011-03-07 15:22:56 +0000

    过滤图片 jc

--- trunk/test/src/com/test/util/New.java
+++ trunk/test/src/com/test/util/New.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -61,7 +63,76 @@ public class New {
     	String str =null;
     	str+=New.getHtmlSource("http://www.oksports.com.cn/1000.htm"); 
     	
+    	String start_str = "<table width=\"950\" border=\"0\" align=\"center\" cellpadding=\"5\" cellspacing=\"5\" class=\"tdbk\">";
+    	String end_str = "<p align=\"center\"><script" ;
+    	int start = str.indexOf(start_str);
+    	int end = str.indexOf(end_str);
+    	
+    	System.out.println(start +"==="+ end);
+    	
+    	str = str.substring(start, end);
     	System.out.println(str);
+    	
+    	//img大写的时候
+    	String img_start_str = "<IMG ";
+		String img_end_str = ">";
+    	List imgList =  new ArrayList();
+    	
+    	StringBuffer buf = new StringBuffer();
+    	
+    	for(int i= 0;i != -1;i = str.indexOf(img_start_str)){
+    		if(str.indexOf(img_start_str)>-1){
+    			int img_start = str.indexOf(img_start_str);
+    			buf.append(str.substring(0, img_start));
+    			
+    			String tmp = str.substring(img_start); 
+    			System.out.println("img 信息" +tmp);
+    			
+    			int img_end = tmp.indexOf(img_end_str);  
+    			String imgTmp = tmp;
+    			tmp = tmp.substring(0,img_end+img_end_str.length());		
+    			buf.append(imgTmp.substring(img_end+img_end_str.length()));    			
+    			
+    			
+    			imgList.add(tmp);    			
+    			str = buf.toString();    			
+    		}else{
+    			System.out.println("break");
+    			break;
+    		}    		
+    	}	
+    	
+    	
+    	//img小写的时候
+    	img_start_str = "<img ";
+		img_end_str = ">";
+    	
+		buf = new StringBuffer();
+    	
+    	for(int i= 0;i != -1;i = str.indexOf(img_start_str)){
+    		if(str.indexOf(img_start_str)>-1){
+    			int img_start = str.indexOf(img_start_str);
+    			buf.append(str.substring(0, img_start));
+    			
+    			String tmp = str.substring(img_start); 
+    			System.out.println("img 信息" +tmp);
+    			
+    			int img_end = tmp.indexOf(img_end_str);  
+    			String imgTmp = tmp;
+    			tmp = tmp.substring(0,img_end+img_end_str.length());		
+    			buf.append(imgTmp.substring(img_end+img_end_str.length()));    			
+    			
+    			
+    			imgList.add(tmp);    			
+    			str = buf.toString();    			
+    		}else{
+    			System.out.println("break");
+    			break;
+    		}    		
+    	}   	
+    	
+    	
+    	System.out.println("最终入库"+str);  	
     	
     }   
 

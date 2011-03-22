fdd4a03b52bc3b295ab7c9ea528145a6ba67ca17
loveecody@foxmail.com <loveecody@foxmail.com@6d9ef304-1be9-e63d-85db-14d8c94b65ce>
2011-03-22 12:00:08 +0000

    NEWS   WJC

--- trunk/test/src/com/test/news/vo/NewsTitleBean.java
+++ trunk/test/src/com/test/news/vo/NewsTitleBean.java
@@ -1,21 +1,25 @@
 package com.test.news.vo;
 
-public class NewsListBean {
+public class NewsTitleBean {
 	// <li>[<a href="?id2=109" style="color:Blue;">英超</a>] &nbsp;<a
 	// href="376499.htm" target="_blank" style="">曼城得狂人示好或换曼奇尼 欲挖迪福取代特维斯</a>
 	// <span class="date">[03月14日 16:57 阅读：160]</span>
 	// </li>
 
-	//新闻类别
+	// 自定义id
+	private int id;
+	// 新闻类别
 	private String type;
-	//类别链接
+	// 类别链接
 	private String typeLink;
-	//新闻标题
+	// 新闻标题
 	private String title;
-	//新闻详细内容连接
+	// 新闻详细内容连接
 	private String titleLink;
-	//日期
+	// 日期
 	private String date;
+	// 详细信息
+	private String info;
 
 	public String getType() {
 		return type;
@@ -55,6 +59,22 @@ public class NewsListBean {
 
 	public void setDate(String date) {
 		this.date = date;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
 	}
 
 }

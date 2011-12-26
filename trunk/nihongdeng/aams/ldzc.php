5c0d10ae19c0b35335f73ca1519aec8d1fb60132
diacainiao@126.com <diacainiao@126.com@6d9ef304-1be9-e63d-85db-14d8c94b65ce>
2011-12-26 09:03:30 +0000

    修改

--- trunk/nihongdeng/aams/ldzc.php
+++ trunk/nihongdeng/aams/ldzc.php
@@ -1,14 +1,14 @@
 <?php
 require_once("action/checkAamsLogin.php");
 require_once("action/smarty_inc.php");
-require_once("../action/mysql.class.php");
+require_once("action/mysql.class.php");
 
-$infoen = getRows(" cp where remark='ContactUs'" ,$db," cp_info_value_en ");
+$infoen = getRows(" cp where remark='Word' " ,$db," cp_info_value_en ");
 $smarty->assign("infoen",html_entity_decode($infoen[0][cp_info_value_en]));
 
-$infocn = getRows(" cp where remark='ContactUs'" ,$db," cp_info_value_zh_cn ");
+$infocn = getRows(" cp where remark='Word' " ,$db," cp_info_value_zh_cn ");
 $smarty->assign("infocn",html_entity_decode($infocn[0][cp_info_value_zh_cn]));
 
 //print_r($infocn[0]);
-$smarty->display("contactus.html");
+$smarty->display("ldzc.html");
 ?>

d19432ea6a8aa260119abe964ca1df13b9c0ea89
loveecody@foxmail.com <loveecody@foxmail.com@6d9ef304-1be9-e63d-85db-14d8c94b65ce>
2012-01-03 12:42:10 +0000

    今天红利=销售额*5%/联盟总分红权数

--- hongli/aams/inc/tab.php
+++ hongli/aams/inc/tab.php
@@ -159,7 +159,7 @@ for(i=0;i<cs.length;i++){
           </tr>
           <tr>
             <td width="30%" height="26" background="../images/tab/tab_14.gif" class="STYLE1"><div align="right" class="STYLE2 STYLE1">½סּל÷לְû</div></td>
-            <td height="18" bgcolor="#FFFFFF" class="STYLE2"><div align="left" class="STYLE2 STYLE1">&nbsp;&nbsp;<?php echo round(((($lmTopInfo[sale_money]+$lmTopInfo[exchange])*0.05)/$lmTopInfo[dayhongli]),2);?></div></td>
+            <td height="18" bgcolor="#FFFFFF" class="STYLE2"><div align="left" class="STYLE2 STYLE1">&nbsp;&nbsp;<?php echo round(((($lmTopInfo[sale_money]+$lmTopInfo[exchange])*0.05)/$lmTopInfo[hongli]),2);?></div></td>
           </tr>
 
           <!--

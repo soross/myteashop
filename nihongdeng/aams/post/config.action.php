17c3c0c27a30c9b51cc9763a87200b66880670f9
diacainiao <diacainiao@126.com>
2012-01-03 08:22:43 +0000

    修改

--- nihongdeng/aams/post/config.action.php
+++ nihongdeng/aams/post/config.action.php
@@ -5,27 +5,27 @@ require_once("../action/mysql.class.php");
 //公司简介中文
 if(isset($_POST[task]) && "updateAboutUsZh_cn"==$_POST[task]){
 
-	$db->query("update comm_code set type_content='".htmlentities($_POST[content])."' where lang='zh_cn' and type_name='AboutUs' ");
+	$db->query("update cp set cp_info_type_zh_cn='".htmlentities($_POST[content])."' where remark='AboutUs' ");
 	echo "<script>alert('公司简介[中文]信息修改成功?');location.href='../aboutme.php';</script>";
 
 //公司简介英文
 }else if(isset($_POST[task]) && "updateAboutUsEn"==$_POST[task]){
-	$db->query("update comm_code set type_content='".htmlentities($_POST[content2])."' where lang='en' and type_name='AboutUs' ");
+	$db->query("update cp set cp_info_type_en='".htmlentities($_POST[content2])."' where remark='AboutUs' ");
 	echo "<script>alert('公司简介[英文]信息修改成功?');location.href='../aboutme.php';</script>";
 
 //公司文化中文
 }else if(isset($_POST[task]) && "updateCultureZh_cn"==$_POST[task]){
-	$db->query("update comm_code set type_content='".htmlentities($_POST[content])."' where lang='zh_cn' and type_name='Culture' ");
+	$db->query("update cp set cp_info_type_zh_cn='".htmlentities($_POST[content])."' where remark='Culture' ");
 	echo "<script>alert('公司文化[中文]信息修改成功?');location.href='../culture.php';</script>";
 
 //公司文化英文
 }else if(isset($_POST[task]) && "updateCultureEn"==$_POST[task]){
-	$db->query("update comm_code set type_content='".htmlentities($_POST[content2])."' where lang='en' and type_name='Culture' ");
+	$db->query("update cp set cp_info_type_en='".htmlentities($_POST[content2])."' where remark='Culture' ");
 	echo "<script>alert('公司文化[英文]信息修改成功?');location.href='../culture.php';</script>";
 
 //品牌诠释
 }else if(isset($_POST[task]) && "updateBrandZh_cn"==$_POST[task]){
-	$db->query("update comm_code set type_content='".htmlentities($_POST[content])."' where lang='zh_cn' and type_name='Brand' ");
+	$db->query("update cp set type_content='".htmlentities($_POST[content])."' where lang='zh_cn' and type_name='Brand' ");
 	echo "<script>alert('品牌诠释[中文]信息修改成功?');location.href='../brand.php';</script>";
 
 }else if(isset($_POST[task]) && "updateBrandEn"==$_POST[task]){

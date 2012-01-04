b8c13e067cb1d78f1400dfa4f9652192c67665ef
diacainiao@126.com <diacainiao@126.com@6d9ef304-1be9-e63d-85db-14d8c94b65ce>
2012-01-04 05:57:22 +0000

    修改

--- trunk/nihongdeng/aams/post/config.action.php
+++ trunk/nihongdeng/aams/post/config.action.php
@@ -5,49 +5,49 @@ require_once("../action/mysql.class.php");
 //公司简介中文
 if(isset($_POST[task]) && "updateAboutUsZh_cn"==$_POST[task]){
 
-	$db->query("update cp set cp_info_type_zh_cn='".htmlentities($_POST[content])."' where remark='AboutUs' ");
+	$db->query("update cp set cp_info_value_zh_cn='".htmlentities($_POST[content])."' where remark='AboutUs' ");
 	echo "<script>alert('公司简介[中文]信息修改成功?');location.href='../aboutme.php';</script>";
 
 //公司简介英文
 }else if(isset($_POST[task]) && "updateAboutUsEn"==$_POST[task]){
-	$db->query("update cp set cp_info_type_en='".htmlentities($_POST[content2])."' where remark='AboutUs' ");
+	$db->query("update cp set cp_info_value_en='".htmlentities($_POST[content2])."' where remark='AboutUs' ");
 	echo "<script>alert('公司简介[英文]信息修改成功?');location.href='../aboutme.php';</script>";
 
 //公司文化中文
 }else if(isset($_POST[task]) && "updateCultureZh_cn"==$_POST[task]){
-	$db->query("update cp set cp_info_type_zh_cn='".htmlentities($_POST[content])."' where remark='Culture' ");
+	$db->query("update cp set cp_info_value_zh_cn='".htmlentities($_POST[content])."' where remark='Culture' ");
 	echo "<script>alert('公司文化[中文]信息修改成功?');location.href='../culture.php';</script>";
 
 //公司文化英文
 }else if(isset($_POST[task]) && "updateCultureEn"==$_POST[task]){
-	$db->query("update cp set cp_info_type_en='".htmlentities($_POST[content2])."' where remark='Culture' ");
+	$db->query("update cp set cp_info_value_en='".htmlentities($_POST[content2])."' where remark='Culture' ");
 	echo "<script>alert('公司文化[英文]信息修改成功?');location.href='../culture.php';</script>";
 
-//品牌诠释
-}else if(isset($_POST[task]) && "updateBrandZh_cn"==$_POST[task]){
-	$db->query("update cp set type_content='".htmlentities($_POST[content])."' where lang='zh_cn' and type_name='Brand' ");
-	echo "<script>alert('品牌诠释[中文]信息修改成功?');location.href='../brand.php';</script>";
+//领导致辞
+}else if(isset($_POST[task]) && "updateldzcZh_cn"==$_POST[task]){
+	$db->query("update cp set cp_info_value_zh_cn='".htmlentities($_POST[content])."' where remark='Word' ");
+	echo "<script>alert('领导致辞[中文]信息修改成功?');location.href='../ldzc.php';</script>";
 
-}else if(isset($_POST[task]) && "updateBrandEn"==$_POST[task]){
-	$db->query("update comm_code set type_content='".htmlentities($_POST[content2])."' where lang='en' and type_name='Brand' ");
-	echo "<script>alert('品牌诠释[英文]信息修改成功?');location.href='../brand.php';</script>";
+}else if(isset($_POST[task]) && "updateldzcEn"==$_POST[task]){
+	$db->query("update cp set cp_info_value_en='".htmlentities($_POST[content2])."' where remark='Word' ");
+	echo "<script>alert('领导致辞[英文]信息修改成功?');location.href='../ldzc.php';</script>";
 
-//加盟代理
-}else if(isset($_POST[task]) && "updateAgentZh_cn"==$_POST[task]){
-	$db->query("update comm_code set type_content='".htmlentities($_POST[content])."' where lang='zh_cn' and type_name='Agent' ");
-	echo "<script>alert('加盟代理[中文]信息修改成功?');location.href='../agent.php';</script>";
+//发展历程
+}else if(isset($_POST[task]) && "updatefzlcZh_cn"==$_POST[task]){
+	$db->query("update cp set cp_info_value_zh_cn='".htmlentities($_POST[content])."' where remark='History' ");
+	echo "<script>alert('加盟代理[中文]信息修改成功?');location.href='../fzlc.php';</script>";
 
-}else if(isset($_POST[task]) && "updateAgentEn"==$_POST[task]){
-	$db->query("update comm_code set type_content='".htmlentities($_POST[content2])."' where lang='en' and type_name='Agent' ");
-	echo "<script>alert('加盟代理[英文]信息修改成功?');location.href='../agent.php';</script>";
+}else if(isset($_POST[task]) && "updatefzlcEn"==$_POST[task]){
+	$db->query("update cp set cp_info_value_en='".htmlentities($_POST[content2])."' where remark='History' ");
+	echo "<script>alert('发展历程[英文]信息修改成功?');location.href='../fzlc.php';</script>";
 
 //联系我们
 }else if(isset($_POST[task]) && "updateContactUsZh_cn"==$_POST[task]){
-	$db->query("update comm_code set type_content='".htmlentities($_POST[content])."' where lang='zh_cn' and type_name='ContactUs' ");
+	$db->query("update cp set cp_info_value_zh_cn='".htmlentities($_POST[content])."' where remark='ContactUs' ");
 	echo "<script>alert('联系我们[中文]信息修改成功?');location.href='../contactus.php';</script>";
 
 }else if(isset($_POST[task]) && "updateContactUsEn"==$_POST[task]){
-	$db->query("update comm_code set type_content='".htmlentities($_POST[content2])."' where lang='en' and type_name='ContactUs' ");
+	$db->query("update cp set cp_info_value_en='".htmlentities($_POST[content2])."' where remark='ContactUs' ");
 	echo "<script>alert('联系我们[英文]信息修改成功?');location.href='../contactus.php';</script>";
 
 }

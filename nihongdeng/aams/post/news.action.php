e381d53a18c40850c468fe2c6f7241abba7221ad
loveecody <loveecody@foxmail.com>
2012-01-04 09:06:55 +0000

    新闻

--- nihongdeng/aams/post/news.action.php
+++ nihongdeng/aams/post/news.action.php
@@ -2,16 +2,22 @@
 require_once("../action/checkAamsLogin.php");
 require_once("../action/mysql.class.php");
 if(isset($_POST[task]) && "addNews"==$_POST[task]){
-	$db->query("insert into lm_news(title,type,author,content,create_date) values('$_POST[title]','$_POST[type]','$_POST[author]','".addslashes($_POST[content])."',now())");
+	$db->query("insert into news(title,type_id,author,content,create_date,lang) values('$_POST[title]','$_POST[type]'," .
+			"'$_POST[author]','".replaceContent($_POST[content])."',now(),'$_POST[lang]')");
 	echo "<script>if(confirm('新闻发布成功,是否继续发布?')){location.href='../addnews.php';}else{location.href='../news.php';}</script>";
 }else if(isset($_GET[task]) && "deleteNews"==$_GET[task]){
-	$db->query("delete from lm_news where id = '$_GET[newsid]'");
+	$db->query("delete from news where id = '$_GET[newsid]'");
 	echo "<script>alert('新闻删除成功?');location.href='../news.php';</script>";
 }else if(isset($_GET[task]) && "toUpdateNews"==$_GET[task]){
 	echo "<script>location.href='../updatenews.php?newsid=".$_GET[newsid]."';</script>";
 }else if(isset($_POST[task]) && "updateNews"==$_POST[task]){
-	$db->query("update lm_news set title='$_POST[title]',type='$_POST[type]',author='$_POST[author]',content='".addslashes($_POST[content])."' where id='".$_POST[newsid]."'");
+	$db->query("update news set title='$_POST[title]',type_id='$_POST[type]',author='$_POST[author]'," .
+			"content='".replaceContent($_POST[content])."',lang='$_POST[lang]' where id='".$_POST[newsid]."'");
 	echo "<script>if(confirm('新闻修改成功,是否继续修改?')){location.href='../updatenews.php?newsid=".$_POST[newsid]."';}else{location.href='../news.php';}</script>";
 }
 
+
+function replaceContent($str){
+	return str_replace("'","&acute;",$str);
+}
 ?>

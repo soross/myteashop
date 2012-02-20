7958c27b9924266b924ae51c5e41a11fcca8dc29
loveecody@foxmail.com <loveecody@foxmail.com@6d9ef304-1be9-e63d-85db-14d8c94b65ce>
2012-02-20 07:47:10 +0000

    fdsa

---
+++ fdy/about.php
@@ -0,0 +1,148 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=GBK" />
+<title>简介 &lt; 关于 &lt; 视点网络(视点工作室)——深圳、广州、上海、杭州、东莞地区专业网站建设服务商,网页设计知名品牌,服务范围涉及企业品牌网站建设、界面风格设计、行业门户构建、WEB系统开发等</title>
+<link href="css/css.css" rel="stylesheet" type="text/css">
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+
+</head>
+<body id="mainPage" class="page235">
+
+<script src="js/common.js" type="text/javascript"></script>
+<script src="js/mt.js" type="text/javascript"></script>
+<script src="js/reflection.js" type="text/javascript"></script>
+<script language="javascript">
+lytebox_doAnimations=true;
+</script>
+<script type="text/javascript" language="javascript" src="js/lytebox.js"></script>
+<link rel="stylesheet" href="css/lytebox.css" type="text/css" media="screen" />
+<div id="container">
+<div id="innerContainer">
+<div id="head">
+  <div id="logo"><a href="\">
+    <img src='images/logo.png'/>
+    </a></div>
+  <div id="quickBar">
+    <ul>
+
+      <li id="quickBar_home"><a href="/index.html">网站首页</a></li>
+
+    </ul>
+  </div>
+  <div id="navigation">
+    <ul>
+      <li id="hello">今天是2012-2-3, 欢迎访问本网站!</li>
+      <li id="navigation_home"><a href="/index.html">网站首页</a></li>
+      <li id='navigation_193'><a href='/html/case.html' class='menulink'>案例</a><ul><li id='module198'><a href='/html/case_web.html'>企业品牌网站建设</a></li><li id='module199'><a href='/html/case_design.html'>界面风格设计</a></li><li id='module200'><a href='/html/case_portal.html'>行业门户构建</a></li><li id='module263'><a href='/html/case_system.html'>WEB系统开发</a></li></ul></li><li id='navigation_196'><a href='/html/service.html' class='menulink'>服务</a><ul><li id='module238'><a href='/html/service_web.html'>企业品牌网站建设</a></li><li id='module261'><a href='/html/service_design.html'>界面风格设计</a></li><li id='module262'><a href='/html/service_portal.html'>行业门户构建</a></li><li id='module264'><a href='/html/service_system.html'>WEB系统开发</a></li><li id='module266'><a href='/html/service_flow.html'>服务流程</a></li></ul></li><li id='navigation_212'><a href='/html/about.html' class='menulink'>关于</a><ul><li id='module235'><a href='/html/about_info.html'>简介</a></li><li id='module236'><a href='/html/about_view.html'>观点</a></li><li id='module237'><a href='/html/about_advantage.html'>优势</a></li><li id='module260'><a href='/html/about_cooperation.html'>加盟&合作</a></li></ul></li><li id='navigation_197'><a href='/html/contact.html' class='menulink'>联络</a><ul><li id='module232'><a href='/html/faq.html'>常见问题</a></li></ul></li><li id='navigation_265'><a href='/html/wiki.html' class='menulink'>知识</a><ul><li id='module270'><a href='/html/main_270.html'>网站策划</a></li><li id='module272'><a href='/html/main_272.html'>网页设计</a></li><li id='module267'><a href='/html/main_267.html'>系统开发</a></li><li id='module269'><a href='/html/main_269.html'>推广优化</a></li><li id='module271'><a href='/html/main_271.html'>其他分类</a></li></ul></li>
+    </ul>
+  </div>
+</div>
+<script type="text/javascript">
+window.addEvent('domready', function(){
+	if ($("navigation_212")) {
+		$("navigation_212").className="navigationOverStyle";
+	}
+	if ($("menu_212")) {
+		$("menu_212").className="menuOverStyle";
+	}
+	if ($$("#menu #module235").length>0) {
+		$$("#menu #module235")[0].className="moduleOverStyle noOverStyle";
+	}
+});
+tagEventEffect(sfHover,"li", "navigation");
+</script>
+    <div id="body">
+      <div id='banner'><div id='picShow_source'><ul><li><p class='pic'><img src='images/content/typeSet212_2010217163051.jpg'/></p><p class='text'>专注@从不懈怠——我们深知，满足您的真正需求并非易事，我们始终以您的角度关注用户的内心所需，凭借专业技术和项目质量控制体系，想你所未想，以领先业界的专业见解为您制定专属方案...这就是我们的态度：专注@从不懈怠。</p></li></ul></div></div>
+      <div id="menu">
+
+
+<ul>
+<li id="menu_home"><a href="/index.html">网站首页</a></li>
+<li id='menu_212'><a href='/html/about.html' class='menulink'>关于</a><ul><li id='module235'><a href='/html/about_info.html'>简介</a></li><li id='module236'><a href='/html/about_view.html'>观点</a></li><li id='module237'><a href='/html/about_advantage.html'>优势</a></li><li id='module260'><a href='/html/about_cooperation.html'>加盟&合作</a></li></ul></li>
+</ul>
+
+<script type="text/javascript">
+window.addEvent('domready', function(){
+
+	//左边菜单的动态效果
+	var menuAction = $$('#menu li.menuOverStyle li a');
+	menuAction.each(function(element) {
+		var fx = new Fx.Styles(element, {duration:200, wait:false});
+
+		element.addEvent('mouseleave', function(){
+			fx.start({
+				'margin-right': 10
+			});
+		});
+
+		element.addEvent('mouseenter', function(){
+			fx.start({
+				'margin-right': 0
+			});
+		});
+	});
+	//为信息列表添加动态事件
+	if ($("textList_simple")) {
+		tagEventEffect(sfHover,"li", "textList_simple");
+	}
+	if ($("picList_sum")) {
+		tagEventEffect(sfHover,"li", "picList_sum");
+	}
+
+
+});
+</script>
+
+      </div>
+      <div id="mainBody">
+              <div class='clearfloat'></div>
+        <div id="position"><a href='main.asp?typeCode=212&id_module=212,235,236,237,260'>关于</a> &gt; 简介</div>
+        <div id="title">简介</div>
+        <div id="content">
+          <div class='content'><h2>视点简介</h2>
+<blockquote>
+<p>&ldquo;视点网络(视点工作室)&rdquo;是一个融创意、设计、开发于一体的专业网站建设服务商，业务内容涉及企业网站建设、门户网站开发、电子商务及WEB应用系统构建。</p>
+<p>我们有着丰富的实践经验、精良的专业技术和高度的敬业精神，在网络服务陷入白热化竞争的今天，我们依然遵循可用性原则，坚持为企业建设简洁大气、好用易用的网站。</p>
+<p>我们的专业网站工程师在各项建站技能上均有超卓的表现，包括 HTML、Javascript、CSS、ASP 及 Databases 等等。在每一个重要的工序上，我们均会和您有紧密的联系，务使每一个细节均能尽善尽美。</p>
+</blockquote>
+<hr />
+<h2>视点文化</h2>
+<blockquote>
+<p>积极主动追求卓越 <br />视点网络从不以现有的成就自满，精益求精是每位成员面对自己工作时的基本态度，也是视点网络得以不断成长的原动力。</p>
+<p>重视团队合作精神 <br />视点网络以专业团队来完成每位客户的托付，团队成员彼此间的默契与合作精神是视点网络赢取客户信任以及成功的重要关键因素。</p>
+<p>客户服务导向 <br />视点网络所提供的产品及服务，向来以协助客户提升网络形象及竞争优势为核心宗旨，不仅是解决客户问题的好伙伴，更是支持客户成长的忠诚力量。</p>
+<p>勇于面对市场挑战 <br />视点网络兢兢业业地面对市场变化，以专业素养、坚定的毅力以及迅速的应变能力，面对市场不断的变化与挑战。</p>
+</blockquote>
+<hr />
+<h2>服务宗旨</h2>
+<blockquote>
+<p>"更高、更快、更贴心" 是我们的服务宗旨，也是對客户的承诺。</p>
+<p>「更高」 <br />我们的专业程序工程师及富有创意的设计师均有十足信心为客户提供高品质的网站建设服务，让客户的投入物超所值！</p>
+<p>「更快」 <br />急客户之所急，出色而高效率的完成客户交付的任务。</p>
+<p>「更贴心」 <br />凭借我们丰富的经验，针对不同客户不同需求，给予专业意见配合他们的经营手法，用我们的创意及技术诚意地为每一位客户创建出贴心的网站，帮助客户建立良好的商业形象。</p>
+</blockquote></div><div class='content'></div>
+        </div>
+      </div>
+      <div class="clearfloat"></div>
+    </div>
+    <br class="clearfloat" />
+<div id="foot">
+  <div id='link'><ul><li><a href='http://www.vdesign.net' target='_blank'>专业网站建设</a></li><li><a href='http://www.vdesign.net' target='_blank'>网站建设工作室</a></li><li><a href='http://www.vdesign.net' target='_blank'>标准化网页设计</a></li><li><a href='http://www.vdesign.net' target='_blank'>标准化网站建设</a></li><li><a href='http://www.vdesign.net/html/service_web.html' target='_blank'>品牌网站建设</a></li><li><a href='http://www.vdesign.net/html/service_design.html' target='_blank'>界面风格设计</a></li><li><a href='http://www.vdesign.net/html/service_portal.html' target='_blank'>行业门户构建</a></li><li><a href='http://www.vdesign.net/html/service_system.html' target='_blank'>WEB系统开发</a></li></ul></div>
+  <div id="copyRight">
+  <!--<div id="sznet110"><a href="http://www.sznet110.gov.cn/webrecord/innernet/Welcome.jsp?bano=44030000637049" title="" target="_blank"><img src="/pic/sznet110anwang.gif" alt=""/></a></div>-->
+    <div id="company">&copy;2002-2010 “视点工作室”版权所有 | <a href="http://www.miibeian.gov.cn/" target="_blank">粤ICP备11054982号-1</a> |
+      <script src="http://s120.cnzz.com/stat.php?id=1233224&web_id=1233224" language="JavaScript" charset="gb2312"></script>
+    </div>
+    <div id="author"><a href="http://www.vdesign.net" target="_blank">Site by vdesign.net</a></div>
+  </div>
+</div>
+</div>
+</div>
+
+</body>
+</html>
+
+
---
+++ fdy/caseinfo.php
@@ -0,0 +1,199 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=GBK" />
+<meta name='keywords' content='视点工作室（深圳,广州,上海,东莞）-专业网站建设,品牌网站建设,界面风格设计,信息门户构建,WEB系统开发,网站建设知名品牌,网站建设工作室' />
+<meta name='description' content='多年来，“视点工作室”全心致力于企业品牌网站建设、界面风格设计和WEB应用系统开发，用我们的专业技术和专注精神为众多客户提供了量身定做的解决方案。' />
+
+<title>企业品牌网站建设 &lt; 案例 &lt; 视点网络(视点工作室)——深圳、广州、上海、杭州、东莞地区专业网站建设服务商,网页设计知名品牌,服务范围涉及企业品牌网站建设、界面风格设计、行业门户构建、WEB系统开发等</title>
+<link href="css/css.css" rel="stylesheet" type="text/css">
+<meta name="keywords" content="视点工作室（深圳,广州,上海,东莞）-专业网站建设,品牌网站建设,界面风格设计,信息门户构建,WEB系统开发,网站建设知名品牌,网站建设工作室" />
+<meta name="description" content="多年来，“视点工作室”全心致力于企业品牌网站建设、界面风格设计和WEB应用系统开发，用我们的专业技术和专注精神为众多客户提供了量身定做的解决方案。" />
+
+<style type="text/css">
+#body #menu dl ul li#module198 {
+}
+#body #menu dl ul li#module198 a {
+}
+</style>
+</head>
+<body id="detailPage" class="typeCode193">
+
+<script src="js/common.js" type="text/javascript"></script>
+<script src="js/mt.js" type="text/javascript"></script>
+<script src="js/reflection.js" type="text/javascript"></script>
+<script language="javascript">
+lytebox_doAnimations=true;
+</script>
+<script type="text/javascript" language="javascript" src="js/lytebox.js"></script>
+<link rel="stylesheet" href="css/lytebox.css" type="text/css" media="screen" />
+<div id="container">
+<div id="innerContainer">
+<div id="head">
+  <div id="logo"><a href="\">
+    <img src='images/logo.png'/>
+    </a></div>
+  <div id="quickBar">
+    <ul>
+
+      <li id="quickBar_home"><a href="/index.html">网站首页</a></li>
+
+    </ul>
+  </div>
+  <div id="navigation">
+    <ul>
+      <li id="hello">今天是2012-2-3, 欢迎访问本网站!</li>
+      <li id="navigation_home"><a href="/index.html">网站首页</a></li>
+      <li id='navigation_193'><a href='/html/case.html' class='menulink'>案例</a><ul><li id='module198'><a href='/html/case_web.html'>企业品牌网站建设</a></li><li id='module199'><a href='/html/case_design.html'>界面风格设计</a></li><li id='module200'><a href='/html/case_portal.html'>行业门户构建</a></li><li id='module263'><a href='/html/case_system.html'>WEB系统开发</a></li></ul></li><li id='navigation_196'><a href='/html/service.html' class='menulink'>服务</a><ul><li id='module238'><a href='/html/service_web.html'>企业品牌网站建设</a></li><li id='module261'><a href='/html/service_design.html'>界面风格设计</a></li><li id='module262'><a href='/html/service_portal.html'>行业门户构建</a></li><li id='module264'><a href='/html/service_system.html'>WEB系统开发</a></li><li id='module266'><a href='/html/service_flow.html'>服务流程</a></li></ul></li><li id='navigation_212'><a href='/html/about.html' class='menulink'>关于</a><ul><li id='module235'><a href='/html/about_info.html'>简介</a></li><li id='module236'><a href='/html/about_view.html'>观点</a></li><li id='module237'><a href='/html/about_advantage.html'>优势</a></li><li id='module260'><a href='/html/about_cooperation.html'>加盟&合作</a></li></ul></li><li id='navigation_197'><a href='/html/contact.html' class='menulink'>联络</a><ul><li id='module232'><a href='/html/faq.html'>常见问题</a></li></ul></li><li id='navigation_265'><a href='/html/wiki.html' class='menulink'>知识</a><ul><li id='module270'><a href='/html/main_270.html'>网站策划</a></li><li id='module272'><a href='/html/main_272.html'>网页设计</a></li><li id='module267'><a href='/html/main_267.html'>系统开发</a></li><li id='module269'><a href='/html/main_269.html'>推广优化</a></li><li id='module271'><a href='/html/main_271.html'>其他分类</a></li></ul></li>
+    </ul>
+  </div>
+</div>
+<script type="text/javascript">
+window.addEvent('domready', function(){
+	if ($("navigation_193")) {
+		$("navigation_193").className="navigationOverStyle";
+	}
+	if ($("menu_193")) {
+		$("menu_193").className="menuOverStyle";
+	}
+	if ($$("#menu #module198").length>0) {
+		$$("#menu #module198")[0].className="moduleOverStyle noOverStyle";
+	}
+});
+tagEventEffect(sfHover,"li", "navigation");
+</script>
+    <div id="body">
+	        <div id='banner'><div id='picShow_source'><ul><li><p class='pic'><img src='images/content/typeSet193_2010218211851.jpg'/></p><p class='text'>重视用户体验的可用性设计原则，高效的项目流程管理和质量控制体系，以及对工作持续的爱好与专注，一直在帮助我们成就更多成功案例...</p></li></ul></div></div>
+      <div id="menu">
+
+
+<ul>
+<li id="menu_home"><a href="/index.html">网站首页</a></li>
+<li id='menu_193'><a href='/html/case.html' class='menulink'>案例</a><ul><li id='module198'><a href='/html/case_web.html'>企业品牌网站建设</a></li><li id='module199'><a href='/html/case_design.html'>界面风格设计</a></li><li id='module200'><a href='/html/case_portal.html'>行业门户构建</a></li><li id='module263'><a href='/html/case_system.html'>WEB系统开发</a></li></ul></li>
+</ul>
+
+<script type="text/javascript">
+window.addEvent('domready', function(){
+
+	//左边菜单的动态效果
+	var menuAction = $$('#menu li.menuOverStyle li a');
+	menuAction.each(function(element) {
+		var fx = new Fx.Styles(element, {duration:200, wait:false});
+
+		element.addEvent('mouseleave', function(){
+			fx.start({
+				'margin-right': 10
+			});
+		});
+
+		element.addEvent('mouseenter', function(){
+			fx.start({
+				'margin-right': 0
+			});
+		});
+	});
+	//为信息列表添加动态事件
+	if ($("textList_simple")) {
+		tagEventEffect(sfHover,"li", "textList_simple");
+	}
+	if ($("picList_sum")) {
+		tagEventEffect(sfHover,"li", "picList_sum");
+	}
+
+
+});
+</script>
+
+      </div>
+      <div id="mainBody">
+              <div class='clearfloat'></div>
+        <div id="position">首页 > <a href='main.asp?typeCode=193&id_module=193,198,199,200,263'>案例</a> &gt; 企业品牌网站建设</div>
+        <div id="title">企业品牌网站建设</div>
+        <div id="content">
+          <div class='title'>顺虹电子股份</div><div id='picShow_thumbs'>
+          <ul>
+          <li>
+          <p class='pic'><a href='images/case/picNews3259_2011114142449.jpg' title='附图' rel='lytebox[plants]' style="background-image:url('images/case/picNews3259_2011114142449.jpg')"><img title='附图' alt='附图' src='images/spacer.gif'/></a></p>
+		  <p class='text'>附图</p></li>
+          <li><p class='pic'><a href='images/case/picNews3259_2011114142449.jpg' title='附图' rel='lytebox[plants]' style="background-image:url('images/case/picNews3259_2011114142449.jpg')"><img title='附图' alt='附图' src='images/spacer.gif'/></a></p><p class='text'>附图</p></li>
+          <li><p class='pic'><a href='images/case/picNews3259_2011114142449.jpg' title='附图' rel='lytebox[plants]' style="background-image:url('images/case/picNews3259_2011114142449.jpg')"><img title='附图' alt='附图' src='images/spacer.gif'/></a></p><p class='text'>附图</p></li>
+          <li><p class='pic'><a href='images/case/picNews3259_2011114142449.jpg' title='附图' rel='lytebox[plants]' style="background-image:url('images/case/picNews3259_2011114142449.jpg')"><img title='附图' alt='附图' src='images/spacer.gif'/></a></p><p class='text'>附图</p></li>
+
+          </ul></div>
+<script type="text/javascript">
+window.addEvent('domready', function(){
+
+	//案例图片组效果(横)
+	var szNormal = 145, szSmall  = 130, szFull   = 200;
+	var kwicks = $$("#picShow_thumbs .pic a");
+	var fx = new Fx.Elements(kwicks, {wait: false, duration: 300, transition: Fx.Transitions.Quad.easeOut});
+	kwicks.each(function(kwick, i) {
+		kwick.addEvent("mouseenter", function(event) {
+			var o = {};
+			o[i] = {width: [kwick.getStyle("width").toInt(), szFull]}
+			kwicks.each(function(other, j) {
+				if(i != j) {
+					var w = other.getStyle("width").toInt();
+					if(w != szSmall) o[j] = {width: [w, szSmall]};
+				}
+			});
+			fx.start(o);
+		});
+		kwick.addEvent("mouseleave", function(event) {
+			var o = {};
+			kwicks.each(function(kwick, i) {
+				o[i] = {width: [kwick.getStyle("width").toInt(), szNormal]}
+			});
+			fx.start(o);
+		})
+	});
+
+
+});
+</script>
+<div class='newsSum'>
+<h4><a href='' title='顺虹电子股份'>顺虹电子股份</a></h4>
+<p>类型：企业品牌网站建设</p>
+<p>客户：深圳顺虹电子股份有限公司</p>
+<p><a href='http://www.sunhomeinc.com' target='_blank'>
+<a href='http://www.sunhomeinc.com' target='_blank'>http://www.sunhomeinc.com</a>
+</a>
+</p></div>
+<div class='content'>
+<p>深圳顺虹电子股份有限公司从创立伊始，便秉持着&ldquo;诚信、创新、专业、专注&rdquo;的经营理念，并致力于成为全球具有领先技术和核心竞争优势的国际化企业！今天，在竞争日益激烈的电子元器件制造业，顺虹电子必将凭借自己非凡的魄力、卓越的胆识、执着的创新精神、先进的管理体系、雄厚的开发能力、优异的产品质量和完善的服务，开创出属于自己的广阔天地！</p>
+<p>深圳市顺虹电子股份有限公司成立于2011年，是一家集研发、生产和销售各类SMD功率电感、插件电感、磁环电感和变压器的高技术型企业，公司已通过ISO9001、ISO14001等相关国际管理体系认证。</p>
+<p>公司主要管理者均拥有该行业10年以上的从业经验，并拥有一只从产品研发、生产到销售的完整、高效的管理团队，主要产品有40多个系列，1000多个品种，广泛应用于通讯、计算机、消费类电子、汽车电子、智能化、工业自动化、医疗器械、安防器材及其他新兴产业领域。深圳工厂厂房面积1200平方米，员工200余人。为促进公司更加快速蓬勃的发展，公司已决定在内地兴建大型工业园，内地工业园目前正在建设中。</p>
+<p>公司秉承&ldquo;诚信、创新、专业、专注&rdquo;的宗旨，竭诚为全球客户提供优质产品和完善服务！</p>
+</div>
+          <div id="toolBar">
+            <ul>
+              <li class="home">
+              <a href="/">返回首页</a>
+              </li>
+              <li class="top"><a href="#">跳到顶部</a></li>
+              <li class="back"><a href="javascript:history.back(-1)">返回上页</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="clearfloat"></div>
+    </div>
+    <br class="clearfloat" />
+<div id="foot">
+  <div id='link'><ul><li><a href='http://www.vdesign.net' target='_blank'>专业网站建设</a></li><li><a href='http://www.vdesign.net' target='_blank'>网站建设工作室</a></li><li><a href='http://www.vdesign.net' target='_blank'>标准化网页设计</a></li><li><a href='http://www.vdesign.net' target='_blank'>标准化网站建设</a></li><li><a href='http://www.vdesign.net/html/service_web.html' target='_blank'>品牌网站建设</a></li><li><a href='http://www.vdesign.net/html/service_design.html' target='_blank'>界面风格设计</a></li><li><a href='http://www.vdesign.net/html/service_portal.html' target='_blank'>行业门户构建</a></li><li><a href='http://www.vdesign.net/html/service_system.html' target='_blank'>WEB系统开发</a></li></ul></div>
+  <div id="copyRight">
+  <!--<div id="sznet110"><a href="http://www.sznet110.gov.cn/webrecord/innernet/Welcome.jsp?bano=44030000637049" title="" target="_blank"><img src="/pic/sznet110anwang.gif" alt=""/></a></div>-->
+    <div id="company">&copy;2002-2010 “视点工作室”版权所有 | <a href="http://www.miibeian.gov.cn/" target="_blank">粤ICP备11054982号-1</a> |
+      <script src="http://s120.cnzz.com/stat.php?id=1233224&web_id=1233224" language="JavaScript" charset="gb2312"></script>
+    </div>
+    <div id="author"><a href="http://www.vdesign.net" target="_blank">Site by vdesign.net</a></div>
+  </div>
+</div>
+</div>
+</div>
+
+</body>
+</html>
+
+

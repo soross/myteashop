cdceedc5242164e99088e49dc43557ab475cac78
loveecody@foxmail.com <loveecody@foxmail.com@6d9ef304-1be9-e63d-85db-14d8c94b65ce>
2012-02-28 03:03:03 +0000

    seo

--- fdy/action/public_info.php
+++ fdy/action/public_info.php
@@ -4,36 +4,16 @@
 $menulist = getList("menu order by seq", $db ," id,url,name,pid,remark");
 $smarty->assign("menulist",$menulist);
 
-/**
-//底部导航
-$nav_buttom_info = getInfo("comm_code where type_name='ButtomNavInfo'",$db,"type_value_".$currlang." as type_value");
-$smarty->assign("ButtomNavInfo",html_entity_decode($nav_buttom_info[type_value]));
+//SEO
+$title = getObject("comm_code where comm_type='SEO_Title'",$db,"comm_value");
+$keyword = getObject("comm_code where comm_type='SEO_Keyword'",$db,"comm_value");
+$desc = getObject("comm_code where comm_type='SEO_DESC'",$db,"comm_value");
+$seo = array();
+$seo['title']=$title[comm_value];
+$seo['keyword']=$keyword[comm_value];
+$seo['desc']=$desc[comm_value];
 
-//版权信息
-$copy_buttom_info = getInfo("comm_code where type_name='ButtomCopyInfo'",$db,"type_value_".$currlang." as type_value");
-$smarty->assign("ButtomCopyInfo",html_entity_decode($copy_buttom_info[type_value]));
-
-
-////modify by 1-8 flash切换
-$flashPic = getRows("comm_code where type_name='TopFlashImage' order by remark",$db);
-$smarty->assign("TopFlashImage",$flashPic);
+$smarty->assign("seo",$seo);
 
 
-
-
-//语言
-if($currlang=="en"){
-	$clang = 'en';
-}else{
-	$clang = '';
-}
-$smarty->assign("clang",$clang);
-
-
-$url=$_SERVER["REQUEST_URI"];
-$smarty->assign("CURL",$url);
-
-
-$smarty->assign("lang",$lang[$currlang]['public']);
-**/
 ?>
--- fdy/index.php
+++ fdy/index.php
@@ -8,21 +8,6 @@ include_once("action/public_info.php");
 $adList = getList("comm_code where comm_type='Index_AD' order by id",$db,"comm_type,comm_value,comm_code ");
 $smarty->assign("adlist",$adList);
 
-/**
-//
-$newslist = getList("news where lang='".$currlang."' order by create_date limit 0,8",$db);
-$smarty->assign("newslist",$newslist);
-
-//∞∏¿˝
-$caselist = getList("obj_case where lang='".$currlang."' order by create_date limit 0,6",$db);
-$smarty->assign("caselist",$caselist);
-
-
-//≤˙∆∑
-$prodlist = getList("product where lang='".$currlang."' order by create_date limit 0,6",$db);
-$smarty->assign("prodlist",$prodlist);
-**/
-
 
 //ºÚΩÈAbout
 $aboutusInfo = getObject("comm_code where comm_code='About'",$db,"comm_value");

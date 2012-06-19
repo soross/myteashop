22bb3d77856944f698c971d9e0a8cb7713e1369c
diacainiao@126.com <diacainiao@126.com@6d9ef304-1be9-e63d-85db-14d8c94b65ce>
2012-06-19 14:17:10 +0000

    修改

--- trunk/boya/action/public_info.php
+++ trunk/boya/action/public_info.php
@@ -1,8 +1,28 @@
 <?php
 //菜单
-$menulist = getRows("boya_type order by indexshow", $db ," id,type_name as menu_name,pid,url,seq ");
-$smarty->assign("menulist",$menulist);
+$indexlist = getRows("boya_type where pid = -1 and seq=1 order by seq,indexshow", $db ," id,type_name as menu_name,pid,url,seq ");
+$smarty->assign("indexlist",$indexlist);
 
+$aboutlist = getRows("boya_type where pid = -1 and seq=5  order by seq,indexshow", $db ," id,type_name as menu_name,pid,url,seq ");
+$smarty->assign("aboutlist",$aboutlist);
+
+$aboutlists = getRows("boya_type where pid = 1 order by seq,indexshow", $db ," id,type_name as menu_name,pid,url,seq ");
+$smarty->assign("aboutlists",$aboutlists);
+
+$trainslist = getRows("boya_type where pid = -1 and seq=2  order by seq,indexshow", $db ," id,type_name as menu_name,pid,url,seq ");
+$smarty->assign("trainslist",$trainslist);
+
+$trainsslist = getRows("boya_type where pid = 2  order by seq,indexshow", $db ," id,type_name as menu_name,pid,url,seq ");
+$smarty->assign("trainsslist",$trainsslist);
+
+$register = getRows("boya_type where pid = -1 and seq=3  order by seq,indexshow", $db ," id,type_name as menu_name,pid,url,seq ");
+$smarty->assign("register",$register);
+
+$registers = getRows("boya_type where pid = 3 order by seq,indexshow", $db ," id,type_name as menu_name,pid,url,seq ");
+$smarty->assign("registers",$registers);
+
+$contact = getRows("boya_type where  pid=-1 and seq=4 order by seq,indexshow", $db ," id,type_name as menu_name,pid,url,seq ");
+$smarty->assign("contact",$contact);
 
 //联系我们
 $contactUs = getInfo("boya_config where comm_type='ContactUs'" ,$db);

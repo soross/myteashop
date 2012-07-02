dca1570008039380ff80caf03bec171f83d0a998
diacainiao <diacainiao@126.com>
2012-07-02 13:43:23 +0000

    新增最近课程

--- boya/index.php
+++ boya/index.php
@@ -14,10 +14,16 @@ $smarty->assign("imagelist",$imagelist);
 $lmlist = getListBySql("select id,type_name as name,url,seq as typeid,index_url  from boya_type where pid='2' order by seq", $db);
 $smarty->assign("lmlist",$lmlist);
 
-if(isset($_GET[typeid])&&!empty($_GET[typeid])){
+//栏目类型--课程
+
+$zxlmlist = getListBySql("select id,type_name as name,url,seq as typeid,index_url  from boya_type where pid='0' order by seq", $db);
+$smarty->assign("zxlmlist",$zxlmlist);
+
+
+if(isset($_GET[typeid])&&!empty($_GET[typeid])&&$_GET[typeid]!=15){
 	$smarty->assign("taskid",$_GET[typeid]);
 }else{
-    $smarty->assign("taskid","5");
+    $smarty->assign("taskid","15");
 }
 
 //图片展示区
@@ -28,7 +34,7 @@ $smarty->assign("piclist",$piclist);
 
 
 $where = " where 1=1 ";
-if(isset($_GET[typeid])&&!empty($_GET[typeid])){
+if(isset($_GET[typeid])&&!empty($_GET[typeid])&&$_GET[typeid]!=15){
 	$where = $where." and type_id ='$_GET[typeid]' ";
 	$infocn = getListBySql("select * from boya_type where id ='$_GET[typeid]'" ,$db);
 	$smarty->assign("infocn",$infocn[0]);
@@ -40,7 +46,7 @@ if(isset($_GET[typeid])&&!empty($_GET[typeid])){
 
 
 //分页
-$pagesize = 5;//一页显示多少条
+$pagesize = 8;//一页显示多少条
 $queryTotal = $db->query("select id,type_id,class_name,open_time,course_name,class_hour,fee,path,content_type,content,isselect,create_date,open_level from boya_course" .$where);
 $total = $db->db_num_rows();
 pageft($total, $pagesize);
@@ -51,8 +57,14 @@ $smarty->assign("page",$pagenav);
 //print_r($where);
 
 //选择栏目列表
+if(isset($_GET[typeid])&&!empty($_GET[typeid])&&$_GET[typeid]!=15){
 $courseRow = getListBySql("select * from boya_course ".$where ."order by create_date desc limit $firstcount, $displaypg " ,$db);
 $smarty->assign("courseRow",$courseRow);
+}else{
+$courseRow = getListBySql("select * from boya_course order by create_date desc limit $firstcount, $displaypg " ,$db);
+$smarty->assign("courseRow",$courseRow);
+
+}
 
 
 

a986a467cf065ea0af5160d54698071461434760
diacainiao@126.com <diacainiao@126.com@6d9ef304-1be9-e63d-85db-14d8c94b65ce>
2012-07-10 15:33:11 +0000

    修改bug和新增需求

--- trunk/boya/post/ddbmpost.php
+++ trunk/boya/post/ddbmpost.php
@@ -1,16 +1,14 @@
 <?php
-include_once("../action/mysql.class.php");
 if(isset($_POST[task]) && md5('RegUser')==$_POST[task]){
-
-
+	include_once("../action/mysql.class.php");
 	$opentime = getListBySql("SELECT c.open_time as open_time FROM boya_course AS c JOIN boya_type AS t ON c.type_id = t.id and c.id=$_POST[course]",$db);
 
 	$opentime = $opentime[0][open_time];
 
-	$db->query("insert into boya_reg_user(username,gender,college,work_unit,mobile,telephone,qq,email,course,create_date,open_time)  " .
+	$db->query("insert into boya_reg_user(username,gender,college,work_unit,mobile,telephone,qq,email,course,create_date,xllb,bmcc,zy,open_time)  " .
 			" value('$_POST[username]','$_POST[sex]','$_POST[certi]','$_POST[address]','$_POST[telephone]','$_POST[phone]','$_POST[qqcode]','$_POST[email]'" .
-			",'$_POST[course]',now(),'$opentime') ");
-	echo "<script>alert('您已报名成功，本校工作人员将48小时内与您联系确认。谢谢！');location.href='../wsbm.php';</script>";
+			",'$_POST[course]',now(),'$_POST[xllb]','$_POST[bmcc]','$_POST[zy]','$opentime') ");
+	echo "<script>alert('您已报名成功，本校工作人员将48小时内与您联系确认。谢谢！');location.href='../ddzxbm.php';</script>";
 }
 ?>
 

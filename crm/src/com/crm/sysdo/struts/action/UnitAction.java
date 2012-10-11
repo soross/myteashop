8f478d9ea8a46895110e1d44139fb4b2e295b830
diacainiao <diacainiao@126.com>
2012-10-11 15:46:45 +0000

    更新

--- crm/src/com/crm/sysdo/struts/action/UnitAction.java
+++ crm/src/com/crm/sysdo/struts/action/UnitAction.java
@@ -15,9 +15,12 @@ import org.apache.struts.action.ActionMapping;
 import org.apache.struts.actions.DispatchAction;
 
 import com.crm.page.PageUtil;
+import com.crm.per.dao.Permission;
 import com.crm.pub.GlobVar;
+import com.crm.pub.PowerKey;
 import com.crm.sysdo.po.TUnit;
 import com.crm.sysdo.service.inf.UnitServiceDao;
+import com.crm.sysdo.struts.form.ToolSaveForm;
 import com.crm.sysdo.struts.form.UnitForm;
 import com.crm.tool.DateUtil;
 import com.mchange.v2.beans.BeansUtils;
@@ -25,7 +28,7 @@ import com.mchange.v2.beans.BeansUtils;
 public class UnitAction extends DispatchAction {
 	
 	private UnitServiceDao unitServiceDao;
-	
+	private Permission perDao;
 	/**
 	 * 跳转往来签约单位列表
 	 * 
@@ -38,18 +41,18 @@ public class UnitAction extends DispatchAction {
 	public ActionForward unitList(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
-		
+		UnitForm unitForm = (UnitForm) form;
 		TUnit tunit = new TUnit();
-		
+		 BeanUtils.copyProperties(tunit, unitForm);
 		Map<String, Object> map = new HashMap<String, Object>();
-		
 		PageUtil pageUtil = new PageUtil(request, this.unitServiceDao
 				.getCount(tunit, map), GlobVar.PAGESIZE_BY_TWENTY_DATA);
 		request.setAttribute("pageUtil", pageUtil);
-		
 		List list = this.unitServiceDao.unitList(tunit, map, pageUtil);
 		request.setAttribute("unitList", list);
-		
+		// 32 角色
+		List sl = perDao.getSonPerList(PowerKey.KEY_UNIT);
+		request.setAttribute("sonPowerByMenu", sl);
 		return new ActionForward("/admin/sysdo/unit/unitlist.jsp");
 	}
 	
@@ -66,7 +69,9 @@ public class UnitAction extends DispatchAction {
 	public ActionForward toAddUnit(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
-		
+		// 32 角色
+		List sl = perDao.getSonPerList(PowerKey.KEY_UNIT);
+		request.setAttribute("sonPowerByMenu", sl);
 		return new ActionForward("/admin/sysdo/unit/addunit.jsp");
 	}
 	
@@ -85,13 +90,13 @@ public class UnitAction extends DispatchAction {
 			throws Exception {
 		UnitForm unitForm =(UnitForm) form;
 		TUnit tunit = new TUnit();
-	    BeanUtils.copyProperties(tunit, unitForm);
+		 BeanUtils.copyProperties(tunit, unitForm);
 	    tunit.setCreateDate(DateUtil.StringToDate_YMD(DateUtil.DateToStringBy_YMD(new Date())));
 	    unitServiceDao.addUnit(tunit);
 	    response.getWriter().write(
 				"<script>location.href='"
 						+ request.getContextPath()
-						+ "/admin/unit.do?task=unitList';</script>");
+						+ "/admin/unit.do?task=unitList&modfunid=85';</script>");
 	    return null;
 	}
 	
@@ -111,6 +116,9 @@ public class UnitAction extends DispatchAction {
 		String id =request.getParameter("id");
 		TUnit tunit = unitServiceDao.get(id);
 		request.setAttribute("unit", tunit);
+		// 32 角色
+		List sl = perDao.getSonPerList(PowerKey.KEY_UNIT);
+		request.setAttribute("sonPowerByMenu", sl);
 		return new ActionForward("/admin/sysdo/unit/updateunit.jsp");
 	}
 	
@@ -129,9 +137,13 @@ public class UnitAction extends DispatchAction {
 	public ActionForward updateUnit(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
+		
 		UnitForm unitForm =(UnitForm) form;
+		
 		TUnit tunit = new TUnit();
 	    BeanUtils.copyProperties(tunit, unitForm);
+	    TUnit tunit1 = unitServiceDao.get(String.valueOf(tunit.getId()));
+	    tunit.setCreateDate(tunit1.getCreateDate());
 	    unitServiceDao.updateUnit(tunit);
 	    response.getWriter().write(
 				"<script>alert('往来签约单位修改成功！');location.href='"
@@ -159,7 +171,7 @@ public class UnitAction extends DispatchAction {
 	    response.getWriter().write(
 				"<script>alert('往来签约单位删除！');location.href='"
 						+ request.getContextPath()
-						+ "/admin/unit.do?task=unitList';</script>");
+						+ "/admin/unit.do?task=unitList&modfunid=85';</script>");
 	    
 	    return null;
 	}
@@ -172,6 +184,16 @@ public class UnitAction extends DispatchAction {
 
 	public void setUnitServiceDao(UnitServiceDao unitServiceDao) {
 		this.unitServiceDao = unitServiceDao;
+	}
+
+
+	public Permission getPerDao() {
+		return perDao;
+	}
+
+
+	public void setPerDao(Permission perDao) {
+		this.perDao = perDao;
 	}
 
 }
